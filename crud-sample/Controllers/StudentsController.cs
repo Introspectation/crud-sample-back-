7af597f9347ae65346e60d2d7ca6476b1c86a142
Introspectation <mert.topkaya1996@gmail.com>
2023-07-14 16:14:22 +0300

    Initial commit

---
+++ crud-sample/Controllers/StudentsController.cs
@@ -0,0 +1,93 @@
+﻿using crud_sample.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace crud_sample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly StudentContext _studentContext;
+        public StudentsController(StudentContext studentContext)
+        {
+            _studentContext = studentContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Student>>>GetStudents()
+        {
+            if (_studentContext.Students == null) 
+            { 
+                return NotFound();
+            }
+            return await _studentContext.Students.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Student>> GetStudent(int id)
+        {
+            if (_studentContext.Students == null)
+            {
+                return NotFound();
+            }
+            var student = await _studentContext.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }          
+            return student;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Student>>PostStudent(Student student)
+        {
+            _studentContext.Students.Add(student);
+            await _studentContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetStudent), new { id = student.ID }, student);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutStudent(int id, Student student)
+        {
+            if(id != student.ID)
+            {
+                return BadRequest();
+            }
+
+            _studentContext.Entry(student).State = EntityState.Modified;
+            try
+            {
+                await _studentContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                throw;
+            }
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteStudent(int id)
+        {
+            if(_studentContext.Students == null)//if there is no record in database
+            {
+                return NotFound();
+            }
+            var student = await _studentContext.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            _studentContext.Students.Remove(student);
+            await _studentContext.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
---
+++ crud-sample/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace crud_sample.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? Birthday { get; set; }
+        public string? Email { get; set; }
+    }
+}
